ad3d81b695cd3df0e767ebb1639f5f6b9e7ea916
cyfco-02 <milvampa1@gmail.com>
2019-08-13 09:16:22 -0500

    test add-numbers.js

--- I.pass-tests/03-sum-numbers/add-numbers.js
+++ I.pass-tests/03-sum-numbers/add-numbers.js
@@ -1,2 +1,24 @@
-function addNumbers(numbers) {}
+// let sum = 0;
+// function addNumbers(numbers) {
+//   for (i = 0; i < numbers.length; i++) {
+//     sum += numbers[i];
+//   }
+//   return sum;
+// }
+
+// function addNumbers(numbers) {
+//   numbers.forEach(number => console.log(number));
+// }
+
+// const suma = addNumbers();
+// numbers => numbers.forEach(number => number + number);
+let sum = 0;
+
+function addNumbers(numbers) {
+  numbers.forEach(function(number) {
+    sum = sum + number;
+  });
+  return sum;
+}
+
 module.exports = addNumbers;
