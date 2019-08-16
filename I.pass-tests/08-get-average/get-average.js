c7b1844858536033c0a56b739fdf08561b6171c7
1000VIA <milvampa1@gmail.com>
2019-08-15 21:54:06 -0500

    08-get-average ready

--- I.pass-tests/08-get-average/get-average.js
+++ I.pass-tests/08-get-average/get-average.js
@@ -1,3 +1,11 @@
 // the input is an array of numbers and strings
 // return the average of all the numbers
 // be sure to exclude the strings
+
+function getNumbers(numbers) {
+    const arrNumbers = numbers.filter(element => typeof element === 'number')
+    const sum = arrNumbers.reduce((previous, current) => current += previous);
+    return sum / arrNumbers.length
+}
+
+module.exports = getNumbers;
